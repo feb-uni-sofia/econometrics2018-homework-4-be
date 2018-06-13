d73760d07d630f4121df5f7c1b00c4a5dcbdc372
elida kacheva <ekacheva7@gmail.com>
2018-06-13 22:55:49 +0300

    :s

--- Problem_1.R
+++ Problem_1.R
@@ -8,11 +8,25 @@ str(houseWork)
 
 ## a)
 
+countM <- length(which(houseWork$sex == 'm')) 
+countF <- length(which(houseWork$sex == 'f'))
+
 ## b)
+
+scoreM <- houseWork[houseWork$sex == 'm', 'hours']
+avgMH <- mean(scoreM)
+
+scoreF <- houseWork[houseWork$sex == 'f', 'hours']
+avgFH <- mean(scoreF)
 
 ## c)
 
+houseWork <- within(houseWork, {female<-ifelse(houseWork$sex == 'f','TRUE','FALSE')})
+houseWork <- within(houseWork, {male<-ifelse(houseWork$sex == 'm','TRUE','FALSE')})
+
 ## d)
+
+fit <- lm(hours ~ female, data = houseWork)
 
 ## e)
 
